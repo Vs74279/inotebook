6c90afd35633375d6aebfc4bde3e349db132791a
vikash kumar singh <drv74279@gmail.com>
2024-03-08 17:15:40 +0530

    useLocation hook in raect

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,13 @@
-import React, { useContext } from "react";
-import NoteContext from "../context/noteContext";
-import { useEffect } from "react";
+import React from "react";
+
+
 
 const ConsumerComponent = () => {
-    // Access the context using useContext hook
-    const a = useContext(NoteContext);
-    useEffect(() => {
-      a.update();
-      // eslint-disable-next-line
-    },[])
-
+    
+    
     return (
         <div>
-          this is about {a.state.name} and he is in class {a.state.class}
+          this is about page
             
         </div>
     );
--- src/context/NoteState.js
+++ src/context/NoteState.js
@@ -1,23 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    // Define state using useState hook
-    const s1= {
-        "name": "vikash",
-        "class": "8a"
-    }
-     const [state, setstate] = useState(s1);
-      const update = () =>{
-        setTimeout(() => {
-         setstate({
-            "name":"singh",
-            "class":"10b"
-         })
-        },1000)
-     }
+    
+     
+      
     return (
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{}}>
            {props.children}
         </NoteContext.Provider>
     );
